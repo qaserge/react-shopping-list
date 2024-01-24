ce82ff430db77a82f7b98e564a46b854894eebf1
Sergei <qaserge@gmail.com>
2024-01-24 14:55:06 -0500

    array num list added

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -4,9 +4,11 @@ export default function Form() {
   return (
     <form className="add-form">
       <select name="" id="">
-        <option value="{1}">1</option>
-        <option value="{2}">2</option>
-        <option value="{3}">3</option>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
       </select>
       <input type="text" placeholder="Item..." />
       <button>Add</button>
