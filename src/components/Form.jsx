1d75af3fbfbe79a475a5d36c9114677ae9100c7e
Sergei <qaserge@gmail.com>
2024-01-25 10:34:47 -0500

    get info from the form and clear form logic added

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,15 +2,24 @@ import React, { useState } from "react";
 
 export default function Form() {
   const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
 
   function handleSubmit(e) {
     e.preventDefault();
-    console.log("Form submitted");
+    if (!description) return;
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    setDescription("");
+    setQuantity(1);
   }
 
   return (
     <form className="add-form" onSubmit={handleSubmit}>
-      <select name="" id="">
+      <select
+        name=""
+        id=""
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
