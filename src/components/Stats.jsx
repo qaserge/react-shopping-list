1008c1deb28559b7b0552fff4d5f5ca322076d49
Sergei <qaserge@gmail.com>
2024-01-27 12:12:58 -0500

    Implemented footer messages

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,9 +1,21 @@
 import React from "react";
 
-export default function Stats() {
+export default function Stats({ items }) {
+  if (!items.length)
+    return (
+      <footer className="stats">Start adding some items to your list!</footer>
+    );
+
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
+
   return (
     <footer className="stats">
-      There are X elements in the list, and Y are already packed.
+      {percentage === 100
+        ? "All items are packed!"
+        : `There are ${numItems} elements in the list, and ${numPacked} are already
+      packed (${percentage}%).`}
     </footer>
   );
 }
